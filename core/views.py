0d45ea1bb178a451a23ae97b7a9f854af5a7c698
PAULO-MICROLINE\dell <desenvolvimento.microline@gmail.com>
2020-01-21 20:38:30 -0400

    Commit da primeira alteração do hellp Django, fazendo a soma de dois numeros

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse("<h1>Alo, {} de {} anos</h1>".format(nome, idade))
+
+def soma(request, num1, num2):
+    var = num1 + num2
+    return HttpResponse("<h1>A soma de {} e {} eh igual a {}</h1>".format(num1, num2, var))
